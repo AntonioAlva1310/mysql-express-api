ee171dabf1cadc6103ed7e6119a0e74f5285e4de
AntonioAlva1310 <smartechbeat@gmail.com>
2022-04-07 20:22:53 -0600

    adding the queries

---
+++ db-init/init.sql
@@ -0,0 +1,19 @@
+CREATE DATABSE IF NOT EXISTS patientsdb;
+
+USE patientsdb;
+
+DROP TABLE IF EXISTS patients;
+
+CREATE TABLE patients ( 
+    patient_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    first_name VARCHAR(255) DEFAULT NULL,
+    last_name VARCHAR(255) DEFAULT NULL,
+    email VARCHAR(255) DEFAULT NULL,
+    phone VARCHAR(255) DEFAULT NULL,
+    address VARCHAR(255) DEFAULT NULL,
+    diagnosis VARCHAR(255) DEFAULT NULL,
+    image_url VARCHAR(255) DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (patient_id),
+    CONSTRAINT uq_patients_email UNIQUE (email)
+);
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import ip from 'ip';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import Response from './domain/response.js'
+import logger from './utils/logger.js'
+
+
+dotenv.config()
+const PORT = process.env.SERVER_PORT || 3000;
+const app = express();
+app.use(cors({ origin: '*' }))
+app.use(express.json());
+app.get('/', (req, res) => res.send(new Response(200, 'OK', 'Patient API,v1.0.0 - All Systems Go')))
+
+
+app.listen(PORT, () => logger.info(`Server running on ${ip.address()}:${PORT}`))
