34d230eb48a3aa4ce84f4f28ea6182c537acc2f0
AntonioAlva1310 <smartechbeat@gmail.com>
2022-05-10 21:55:53 -0600

    Added routes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import Response from './domain/response.js'
 import logger from './utils/logger.js'
 import HttpStatus from './controller/patient.controller.js'
+import patientRoutes from '../src/route/patient.route.js'
 
 
 dotenv.config()
@@ -12,7 +13,10 @@ const PORT = process.env.SERVER_PORT || 3000;
 const app = express();
 app.use(cors({ origin: '*' }))
 app.use(express.json());
+app.use('/patients', patientRoutes);
 app.get('/', (req, res) => res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API,v1.0.0 - All Systems Go')))
+app.all('*', (req, res) => res.satus(HttpStatus.NOT_FOUND.code)
+    .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'Route does not exist on the server')))
 
 
 app.listen(PORT, () => logger.info(`Server running on ${ip.address()}:${PORT}`))
